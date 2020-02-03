4bd279423469ab4fa924046a77b7b322dc2ca5f0
Jonathan Ballard <asgardschosen@gmail.com>
2020-02-02 18:30:43 -0800

    finished project

--- user_assignment.py
+++ user_assignment.py
@@ -7,7 +7,7 @@ class User:
         self.name = username
         self.email = email
         self.balance = 0
-    def make_withdraw(self, amount):
+    def withdraw(self, amount):
         self.balance -= amount
     def deposit(self, amount):
         self.balance += amount
@@ -25,12 +25,24 @@ user3 = User('Francis', 'fran@google.com')
 
 
 
+user1.deposit(550)
+user1.deposit(150)
+user1.deposit(350)
+user1.withdraw(35)
+user1.display_user_balance()
 
+user2.deposit(10)
+user2.deposit(150)
+user2.withdraw(50)
+user2.withdraw(10)
+user2.display_user_balance()
 
+user3.deposit(1110)
+user3.withdraw(150)
+user3.withdraw(50)
+user3.withdraw(10)
+user3.display_user_balance()
 
-
-
-
-
-
-
+user1.transfer_money(user3, 500)
+user1.display_user_balance()
+user3.display_user_balance()
